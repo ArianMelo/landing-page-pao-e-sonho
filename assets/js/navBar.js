400721dc5ace2d2720a97c6d1a848d4237596216
Arian Melo <arianmelo7733@gmail.com>
2024-09-08 00:26:52 -0300

    lading page padaria 0.9v
    Quase Finalizando a Landing page.

---
+++ assets/js/navBar.js
@@ -0,0 +1,10 @@
+
+
+function showNavBar(){
+
+  if (document.getElementById('navBarMenu').style.display == "block") {
+    document.getElementById('navBarMenu').style.display = "none";
+  } else {
+    document.getElementById('navBarMenu').style.display = "block";
+  }
+}
