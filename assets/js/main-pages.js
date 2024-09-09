d89bce2d4c42bcba3253624017673139b909378a
Arian Melo <arianmelo7733@gmail.com>
2024-09-09 13:04:07 -0300

    Final Mudanças v1
    Mudanças no final V1

--- assets/js/main-pages.js
+++ assets/js/main-pages.js
@@ -1,3 +1,9 @@
+document.getElementById('conSel1').style.display = "block";
+document.getElementById('conSel2').style.display = "none";
+document.getElementById('conSel3').style.display = "none";
+document.getElementById('conSel4').style.display = "none";
+document.getElementById('conSel5').style.display = "none";
+document.getElementById('conSel6').style.display = "none";
 
 function conSelBtn1() {
   document.getElementById('conSel1').style.display = "block";
