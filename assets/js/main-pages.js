9a404a8a078e7d55d681193e27900e98d20c8d1d
Arian Melo <arianmelo7733@gmail.com>
2024-09-09 12:48:53 -0300

    Projeto Final
    Fim do projeto Institucional

---
+++ assets/js/main-pages.js
@@ -0,0 +1,54 @@
+
+function conSelBtn1() {
+  document.getElementById('conSel1').style.display = "block";
+  document.getElementById('conSel2').style.display = "none";
+  document.getElementById('conSel3').style.display = "none";
+  document.getElementById('conSel4').style.display = "none";
+  document.getElementById('conSel5').style.display = "none";
+  document.getElementById('conSel6').style.display = "none";
+}
+
+function conSelBtn2() {
+  document.getElementById('conSel1').style.display = "none";
+  document.getElementById('conSel2').style.display = "block";
+  document.getElementById('conSel3').style.display = "none";
+  document.getElementById('conSel4').style.display = "none";
+  document.getElementById('conSel5').style.display = "none";
+  document.getElementById('conSel6').style.display = "none";
+}
+
+function conSelBtn3() {
+  document.getElementById('conSel1').style.display = "none";
+  document.getElementById('conSel2').style.display = "none";
+  document.getElementById('conSel3').style.display = "block";
+  document.getElementById('conSel4').style.display = "none";
+  document.getElementById('conSel5').style.display = "none";
+  document.getElementById('conSel6').style.display = "none";
+}
+
+function conSelBtn4() {
+  document.getElementById('conSel1').style.display = "none";
+  document.getElementById('conSel2').style.display = "none";
+  document.getElementById('conSel3').style.display = "none";
+  document.getElementById('conSel4').style.display = "block";
+  document.getElementById('conSel5').style.display = "none";
+  document.getElementById('conSel6').style.display = "none";
+}
+
+function conSelBtn5() {
+  document.getElementById('conSel1').style.display = "none";
+  document.getElementById('conSel2').style.display = "none";
+  document.getElementById('conSel3').style.display = "none";
+  document.getElementById('conSel4').style.display = "none";
+  document.getElementById('conSel5').style.display = "block";
+  document.getElementById('conSel6').style.display = "none";
+}
+
+function conSelBtn6() {
+  document.getElementById('conSel1').style.display = "none";
+  document.getElementById('conSel2').style.display = "none";
+  document.getElementById('conSel3').style.display = "none";
+  document.getElementById('conSel4').style.display = "none";
+  document.getElementById('conSel5').style.display = "none";
+  document.getElementById('conSel6').style.display = "block";
+}
